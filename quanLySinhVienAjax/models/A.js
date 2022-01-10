ea432bf5f5400f03d7dfedc4eedadc8f4ac4b542
unknown <nguyenhuyhoanglong2016@gmail.com>
2022-01-10 21:24:00 +0700

    deploy

--- quanLySinhVienAjax/models/A.js
+++ quanLySinhVienAjax/models/A.js
@@ -2,11 +2,9 @@
 function codePythonbangJs() {
     console.log('code python = js');
 }
-<<<<<<< HEAD
 
 
 
 console.log('Tui là B hihi')
-=======
 console.log('Mình là A');
->>>>>>> 7db7502cc72ade7273f44bed8cab1aa7c4745851
+console.log('B lần cuối');
